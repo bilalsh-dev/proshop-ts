27191b49201e3a4166ce0ebe7aedd843eca4715d
bilalshahid929 <bilalshahid929@gmail.com>
2023-09-01 15:52:01 +0500

    fix:--> add the custom css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./App.css";
 
 function App() {
   return (
