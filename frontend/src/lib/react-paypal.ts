4c7e8766f9fbea9c3948675ceca1e7e9906c6367
bilalshahid929 <bilalshahid929@gmail.com>
2023-11-11 16:42:51 +0400

    Add the react-paypal-js pkg

---
+++ frontend/src/lib/react-paypal.ts
@@ -0,0 +1,13 @@
+import {
+  PayPalButtons,
+  PayPalScriptProvider,
+  SCRIPT_LOADING_STATE,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
+
+export {
+  PayPalButtons,
+  PayPalScriptProvider,
+  SCRIPT_LOADING_STATE,
+  usePayPalScriptReducer,
+};
