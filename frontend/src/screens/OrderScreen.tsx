688249a42b3a5a9589e14c8eee3907006056b9f7
bilalshahid929 <bilalshahid929@gmail.com>
2023-11-11 16:46:18 +0400

    order screen

--- frontend/src/screens/OrderScreen.tsx
+++ frontend/src/screens/OrderScreen.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Loader, Message } from "components";
 import { useEffect } from "react";
 import {
