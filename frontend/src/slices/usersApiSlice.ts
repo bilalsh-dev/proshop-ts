7da0ce24c4abbc36c0c89fd85e5ff3451b065df2
bilalshahid929 <bilalshahid929@gmail.com>
2023-11-11 16:44:51 +0400

    Add the users api slice

--- frontend/src/slices/usersApiSlice.ts
+++ frontend/src/slices/usersApiSlice.ts
@@ -1,5 +1,10 @@
 import { USERS_URL } from "@/constants";
-import type { LoginPayload, RegisterPayload, UserInfo } from "@/types";
+import type {
+  LoginPayload,
+  RegisterPayload,
+  UpdateUserInfo,
+  UserInfo,
+} from "@/types";
 
 import { apiSlice } from "./apiSlice";
 
@@ -25,8 +30,50 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: "POST",
       }),
     }),
+    profile: builder.mutation<UserInfo, UpdateUserInfo>({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+      }),
+      providesTags: ["User"],
+      keepUnusedDataFor: 5,
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: "DELETE",
+      }),
+    }),
+    getUserDetails: builder.query({
+      query: (id) => ({
+        url: `${USERS_URL}/${id}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["User"],
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
-  usersApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useUpdateUserMutation,
+  useGetUserDetailsQuery,
+} = usersApiSlice;
