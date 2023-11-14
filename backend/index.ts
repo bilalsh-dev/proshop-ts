2dc643f12a4e89ca62b582f919f42f28857ac6c1
bilalshahid929 <bilalshahid929@gmail.com>
2023-11-14 16:16:59 +0400

    Add the upload route

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import path from "path";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
@@ -6,6 +7,7 @@ import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -33,11 +35,17 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) => {
   res.send({
     clientId: process.env.PAYPAL_CLIENT_ID,
   });
 });
+
+const __dirname = path.resolve();
+console.log("__dirname", __dirname);
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`server is running on ${port}`));
